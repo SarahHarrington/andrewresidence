0bcc291a4fbb367eaf933d4bdc1488f863d251b9
Sarah Harrington <sarah.soberg@gmail.com>
2018-03-10 14:41:08 -0600

    updated var

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -27,8 +27,8 @@ if (process.env.DATABASE_URL) {
 
 } else {
   config = {
-    user: process.env.PG_USER || null, //env var: PGUSER
-    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    user: process.env.PG_USER || 'postgres', //env var: PGUSER
+    password: process.env.DATABASE_SECRET || 'postgres', //env var: PGPASSWORD
     host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
     port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
     database: process.env.DATABASE_NAME || 'andrew', //env var: PGDATABASE
